809e200e981853f345cbe30e17110d05f71047a8
nobrain <moviemaker@alfonso>
2023-11-28 21:16:31 +0100

    feat: added markdown preview plugin

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -14,6 +14,12 @@ return{
         lazy = false,
     },
     {
+        "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        ft = { "markdown" },
+        build = function() vim.fn["mkdp#util#install"]() end,
+    },
+    {
         "folke/zen-mode.nvim"
     },
     {
